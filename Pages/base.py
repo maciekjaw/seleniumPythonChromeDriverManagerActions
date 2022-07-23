394249783cd071e0c004505d0d200f5592b55b12
maciekjaw <maciekjaworski@wp.pl>
2022-07-23 16:17:03 +0200

    chrome_options

--- Pages/base.py
+++ Pages/base.py
@@ -1,12 +1,19 @@
 import unittest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
+chrome_options = Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--remote-debugging-port=9222')
 
 class StartSelenium(unittest.TestCase):
     
     def setUp(self) -> None:
-        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(options=chrome_options, service=Service(ChromeDriverManager().install()))
         self.driver.get("http://www.google.com")
     
     def tearDown(self):
