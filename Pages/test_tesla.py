98e571832d13f6d60df233c76fd53f5a4a60b9ca
maciekjaw <33606294+maciekjaw@users.noreply.github.com>
2022-07-26 20:30:41 +0200

    Update test_tesla.py

--- Pages/test_tesla.py
+++ Pages/test_tesla.py
@@ -1,16 +1,19 @@
 import unittest
-import base
+from Pages.test_base import StartSelenium as start
 from selenium.webdriver.common.by import By
 
-class LandingPage(base.StartSelenium, unittest.TestCase):
+SHOP_BTN:str = "tds-site-nav-item-text"
+
+class LandingPage(start, unittest.TestCase):
         
-        def test_go_to_tesla_shop(self, url:str = "https://www.tesla.com", expected_word:str = 'https://shop.tesla.com'):
+        def test_go_to_tesla_shop(self, url:str = "https://www.tesla.com",expected_url:str = 'https://shop.tesla.com'):    
+                acctual_url:str = 'https://shop.tesla.com' 
+        
                 driver = self.driver
                 driver.get(url)
-                driver.find_element(By.CLASS_NAME, "tds-site-nav-item-text").click()
-                self.assertIn(expected_word, driver.current_url)
+                driver.find_element(By.CLASS_NAME, SHOP_BTN).click()
+                get_shop_url = driver.current_url
+                self.assertEqual(get_shop_url, expected_url)
         
-if __name__ == '__main__':
-        unittest.main()
-
-
+if __name__ == "__main__":
+    unittest.main()
