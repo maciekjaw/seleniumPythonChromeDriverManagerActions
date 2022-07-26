785841567842941ff7d831f08ebff2d88e8a8199
maciekjaw <33606294+maciekjaw@users.noreply.github.com>
2022-07-26 20:22:20 +0200

    Update test_tesla.py

--- Pages/test_tesla.py
+++ Pages/test_tesla.py
@@ -7,7 +7,7 @@ class LandingPage(base.StartSelenium, unittest.TestCase):
         def test_go_to_tesla_shop(self, url:str = "https://www.tesla.com", expected_word:str = 'https://shop.tesla.com'):
                 driver = self.driver
                 driver.get(url)
-                driver.find_element(By.LINK_TEXT, "Shop").click()
+                driver.find_element(By.CLASS_NAME, "tds-site-nav-item-text").click()
                 self.assertIn(expected_word, driver.current_url)
         
 if __name__ == '__main__':
